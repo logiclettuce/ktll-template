dbecf6aa1142d00a4b67a229aa08dd592847ebfe
Dušan Uverić <dusanuveric@protonmail.com>
2021-07-18 10:53:53 +0200

    Add DSL for CORS configurationSource and corsConfiguration
    Signed-off-by: Dušan Uverić <dusanuveric@protonmail.com>

--- src/main/kotlin/io/uvera/template/security/configuration/SecurityConfiguration.kt
+++ src/main/kotlin/io/uvera/template/security/configuration/SecurityConfiguration.kt
@@ -2,6 +2,8 @@ package io.uvera.template.security.configuration
 
 import io.uvera.template.security.filter.JwtAuthFilter
 import io.uvera.template.security.service.CustomUserDetailsService
+import io.uvera.template.util.extensions.configurationSource
+import io.uvera.template.util.extensions.corsConfiguration
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.http.HttpMethod
@@ -17,8 +19,6 @@ import org.springframework.security.config.web.servlet.invoke
 import org.springframework.security.crypto.factory.PasswordEncoderFactories
 import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
-import org.springframework.web.cors.CorsConfiguration
-import org.springframework.web.cors.CorsConfigurationSource
 
 @Configuration
 @EnableWebSecurity
@@ -43,8 +43,8 @@ class SecurityConfiguration(
     override fun configure(http: HttpSecurity) {
         http {
             cors {
-                configurationSource = CorsConfigurationSource {
-                    CorsConfiguration().apply {
+                configurationSource {
+                    corsConfiguration {
                         allowedOrigins = listOf("*")
                         allowedHeaders = listOf("*")
                         allowedMethods = listOf(
